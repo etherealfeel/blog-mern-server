d93dde80c1abc699c1ed643c55aa68c36b4bb004
etherealfeel <graciagodsyndrome@gmail.com>
2023-10-06 23:49:12 +0300

    Apply bcrypt hashing

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
-import {validationResult} from 'express-validator'
+import { validationResult } from 'express-validator';
 
 import { registerValidator } from './validations/auth.js';
+
+import UserModel from './models/User.js';
 
 mongoose
   .connect('mongodb+srv://admin:admin@cluster0.oqurahn.mongodb.net/')
@@ -14,15 +17,26 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidator, (req, res) => {
+app.post('/auth/register', registerValidator, async (req, res) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()) {
+  if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
 
-  res.json({
-    success: true
-  })
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
+  });
+
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(4000, (err) => {
