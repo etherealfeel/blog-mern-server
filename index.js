f0f61970833095d6baeb5c18be3e3301bacf2633
etherealfeel <graciagodsyndrome@gmail.com>
2023-10-10 20:17:18 +0300

    Add user controller

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
-import { validationResult } from 'express-validator';
-
-import { registerValidator } from './validations/auth.js';
-
-import UserModel from './models/User.js';
+import { registerValidation, loginValidation } from './validations/validation.js';
+import checkAuth from './utils/checkAuth.js';
+import { register, login, getMe } from './controllers/UserController.js';
 
 mongoose
-  .connect('mongodb+srv://admin:admin@cluster0.oqurahn.mongodb.net/')
+  .connect('mongodb+srv://admin:admin@cluster0.oqurahn.mongodb.net/blog')
   .then(() => console.log('db is working'))
   .catch(() => console.log('error connecting db'));
 
@@ -17,27 +13,9 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidator, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  }
-
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  });
-
-  const user = await doc.save();
-
-  res.json(user);
-});
+app.post('/auth/register', registerValidation, register);
+app.post('/auth/login', loginValidation, login);
+app.get('/auth/me', checkAuth, getMe);
 
 app.listen(4000, (err) => {
   if (err) return console.log(err);
---
+++ validations/validation.js
@@ -0,0 +1,20 @@
+import {body} from 'express-validator'
+
+export const registerValidation = [
+    body('email').isEmail(),
+    body('password').isLength({min: 5}),
+]
+
+export const loginValidation = [
+    body('email').isEmail(),
+    body('password').isLength({min: 5}),
+    body('fullName').isLength({min: 5}),
+    body('avatarUrl').optional().isURL(),
+]
+
+export const postCreateValidation = [
+    body('title').isLength({min: 3}).isString(),
+    body('text').isLength({min: 10}).isString(),
+    body('tags').isLength({min: 5}).optional().isArray(),
+    body('imageUrl').optional().isString(),
+]
